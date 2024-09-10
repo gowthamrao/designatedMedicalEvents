dd8db7735f92046b8bd6ff66fd4dc883dc7c1543
Gowtham Rao <rao@ohdsi.org>
2024-09-10 14:01:43 -0400

    Add script and cohort definition set

---
+++ Script.R
@@ -0,0 +1,83 @@
+projectName <- "ohdsiPlDme"
+baseFolder <- file.path("E:", "studyResults", projectName)
+dir.create(path = baseFolder,
+           showWarnings = FALSE,
+           recursive = TRUE)
+
+activeProjectFolder <- rstudioapi::getActiveDocumentContext()$path |> dirname()
+
+ohdsiPlDmeCohortIds <- c(
+  207,
+  210,
+  211,
+  213,
+  216,
+  218,
+  219,
+  222,
+  229,
+  275,
+  276,
+  720,
+  723,
+  724,
+  725,
+  726,
+  727,
+  728,
+  729,
+  730,
+  731,
+  732,
+  733,
+  735,
+  736,
+  737,
+  739,
+  741,
+  747
+) |> sort() |> unique()
+
+
+
+ROhdsiWebApi::authorizeWebApi(
+  baseUrl = "https://atlas-phenotype.ohdsi.org/WebAPI",
+  authMethod = "db",
+  webApiUsername = Sys.getenv('ohdsiAtlasPhenotypeUser'),
+  webApiPassword = Sys.getenv('ohdsiAtlasPhenotypePassword')
+)
+
+
+#get cohort definition set----
+cohortDefinitionSet <- ROhdsiWebApi::exportCohortDefinitionSet(baseUrl = "https://atlas-phenotype.ohdsi.org/WebAPI",
+                                                               cohortIds = ohdsiPlDmeCohortIds,
+                                                               generateStats = TRUE) |>
+  dplyr::select(colnames(CohortGenerator::createEmptyCohortDefinitionSet()))
+
+saveRDS(
+  object = cohortDefinitionSet,
+  file = file.path(activeProjectFolder, "CohortDefinitionSet.RDS")
+)
+readr::write_excel_csv(x = cohortDefinitionSet,
+                       file = file.path(activeProjectFolder, "CohortDefinitionSet.csv"))
+
+cohortDefinitionSet <- readRDS(file.path(activeProjectFolder, "CohortDefinitionSet.RDS"))
+
+# invoke cohort generation
+cohortTableNames <- CohortGenerator::getCohortTableNames(cohortTable = projectName)
+
+
+OhdsiHelpers::executeCohortGenerationInParallel(
+  cdmSources = cdmSources,databaseIds = "healthverity_cc",
+  cohortDefinitionSet = cohortDefinitionSet,
+  outputFolder = file.path(baseFolder, "CohortGenerator"),
+  cohortTableNames = cohortTableNames
+)
+
+
+OhdsiHelpers::executeCohortDiagnosticsInParallel(
+  cdmSources = cdmSources,
+  cohortDefinitionSet = cohortDefinitionSet,
+  outputFolder = file.path(baseFolder, "CohortDiagnostics"),
+  cohortTableNames = cohortTableNames
+)
